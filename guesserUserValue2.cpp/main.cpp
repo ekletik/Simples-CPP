ed67839c9f210bb889c01c2f4b1fa6e3b2d2e457
Leo N <csneto@leos-macbook-pro.local>
2016-09-09 14:59:47 -0400

    Initial Commit

---
+++ guesserUserValue2.cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  guesserUserValue2.cpp
+//
+//  Created by Leo N. on 2016-09-09.
+//  Copyright © 2016 Leo N. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
